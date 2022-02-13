635ee877d6b2d1cc6d1015c081d1a124bd1e8ba6
Agbeniga <agbenigacaleb@gmail.com>
2022-02-13 16:51:55 +0100

    Constant variables added

--- lib/utils/constants/app_constants.dart
+++ lib/utils/constants/app_constants.dart
@@ -13,3 +13,12 @@ const defaultNetworkImage = '';
 const mainAppBarLogo = AssetImage(
   '',
 );
+
+const String onboardingImage1 = "assets/Images/onboarding_1.svg";
+const String onboardingImage2 = "assets/Images/onboarding_2.svg";
+
+const String onboardingImage3 = "assets/Images/onboarding_3.svg";
+
+const String loginImage = "assets/Images/login_image.svg";
+
+const String verifyEmailImage = "assets/Images/email_verify.svg";
