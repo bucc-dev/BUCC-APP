31ab0126066e3caa0e03c8069b1b016e39025755
Agbeniga <agbenigacaleb@gmail.com>
2022-02-13 16:40:48 +0100

    Created Onboarding Screen

---
+++ lib/view/components/onboarding_component.dart
@@ -0,0 +1,59 @@
+import 'package:bucc_app/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class OnboardingComponent extends StatelessWidget {
+  const OnboardingComponent(
+      {Key? key,
+      required this.imageurl,
+      required this.title,
+      required this.description})
+      : super(key: key);
+
+  final String imageurl, title, description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: SvgPicture.asset(imageurl),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+            ),
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: "Montserrat",
+                fontSize: 22,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 15),
+            child: Text(
+              description,
+              style: const TextStyle(
+                fontFamily: "Montserrat",
+                color: lightGrey,
+                fontSize: 12,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/onboarding_view.dart
@@ -0,0 +1,112 @@
+import 'package:bucc_app/utils/constants/app_constants.dart';
+import 'package:bucc_app/utils/constants/colors.dart';
+import 'package:bucc_app/view/components/button_component.dart';
+import 'package:bucc_app/view/components/onboarding_component.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnboardingView extends StatefulWidget {
+  const OnboardingView({Key? key}) : super(key: key);
+
+  @override
+  _OnboardingViewState createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  final PageController _controller = PageController();
+  int page = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/onboarding_login");
+                  },
+                  child: const Text(
+                    "Skip",
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: black,
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: PageView(
+                controller: _controller,
+                children: const [
+                  OnboardingComponent(
+                    imageurl: onboardingImage1,
+                    title: "Get Updated",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipg elit. Facilisis vitae diam nec lectus lobortis. semperhhjo rfhreskwfl klty.",
+                  ),
+                  OnboardingComponent(
+                    imageurl: onboardingImage2,
+                    title: "Access Resources",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipg elit. Facilisis vitae diam nec lectus lobortis.  semperhhjo rfhreskwfl klty.",
+                  ),
+                  OnboardingComponent(
+                    imageurl: onboardingImage3,
+                    title: "Meet up Deadlines",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipg elit. Facilisis vitae diam nec lectus lobortis.  semperhhjo rfhreskwfl klty.",
+                  ),
+                ],
+              ),
+            ),
+            SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: const ExpandingDotsEffect(
+                dotHeight: 5,
+                activeDotColor: purple,
+              ),
+              onDotClicked: (int index) {
+                setState(() {
+                  _controller.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.easeInOut,
+                  );
+                  page = index;
+                });
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: ButtonComponent(
+                onPressed: () {
+                  if (_controller.page!.toInt() != 2) {
+                    setState(() {
+                      _controller.nextPage(
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.easeInOut,
+                      );
+                      page++;
+                    });
+                  } else {
+                    Navigator.pushNamed(context, "/onboarding_login");
+                  }
+                },
+                text: page != 2 ? "NEXT" : "GET STARTED",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
